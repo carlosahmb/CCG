1788ad462dfb28a7a39bb6213b52dcaebda645c4
CARLOS <carloshilariu@gmail.com>
2019-05-03 12:54:28 -0400

    update 3

--- app/src/main/java/com/app/carlos/ccg/Sugestao.java
+++ app/src/main/java/com/app/carlos/ccg/Sugestao.java
@@ -1,6 +1,8 @@
 package com.app.carlos.ccg;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SwitchCompat;
@@ -9,21 +11,37 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 
-public class Sugestao extends Activity {
+public class Sugestao extends AppCompatActivity {
 
     SwitchCompat switchCompat;
+    Button enviarSugestao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sugestao);
 
-        switchCompat = (SwitchCompat) findViewById(R.id.switchAnomino);
+        switchCompat = findViewById(R.id.switchAnomino);
+        FloatingActionButton btnVoltar = findViewById(R.id.btnVoltar);
+        enviarSugestao = findViewById(R.id.enviarSugestao);
 
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Sugestao.this, MainActivity.class));
+            }
+        });
 
         switchCompat.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+
+        enviarSugestao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
             }
         });
